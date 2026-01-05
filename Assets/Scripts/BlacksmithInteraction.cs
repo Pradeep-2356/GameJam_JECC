364d1f6dca61768515a2508cebf5d84532d87f9a
dhananjay <ppghsharma2020@gmail.com>
2026-01-06 00:27:58 +0530

    BlackSmith Added

---
+++ Assets/Scripts/BlacksmithInteraction.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BlacksmithInteraction : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("IsTalking", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("IsTalking", false);
+        }
+    }
+}
